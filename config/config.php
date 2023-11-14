0199202c7b1556860202facc415d89a5f87af03b
VicenteOdes <olivaresvaldesvicente@gmail.com>
2023-11-13 23:14:09 -0300

    config

--- config/config.php
+++ config/config.php
@@ -3,6 +3,7 @@
 define("KEY", "adminfoodok1");
 define("COD", "AES-128-ECB");
 // Datos de conexión a la base de datos
+//cs.ilab.cl, 2_BD_69, 2_BD_69, 
 define("server","cs.ilab.cl");
 define("user","2_BD_69");
 define("db","2_BD_69");
